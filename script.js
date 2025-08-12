5f6580c3f445e7ecc5f36653a6cddf91ca1234fe
Priyank Bansal <165632145+Bansal-priyank@users.noreply.github.com>
2025-08-12 23:47:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let num1 = prompt("Enter your first number: ");
+let num2 = prompt("Enter your second number: ");
+
+num1 = NUMBER(num1);
+num2 = NUMBER(num2);
+
+alert(num1 === num2);
